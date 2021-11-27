ef6635995757196d5baab1bb4588a5fea4cfb650
Alish Rani <94797867+AlishRani@users.noreply.github.com>
2021-11-27 22:50:34 +0530

    Add files via upload

---
+++ convert degree celsius into degree farhenhiet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+     float cels;
+     printf("enter the value of cels\n");
+     scanf("%f",&cels);
+     float farhen=9/5*cels+32;
+     printf("converted into degree farhen%f\n",farhen);
+     return 0;
+}
